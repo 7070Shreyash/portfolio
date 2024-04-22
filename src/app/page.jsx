f3046a4dafb006b8dafdc8bebda722955b5d19a4
shreyash <shreyashacharya001@gmail.com>
2024-04-23 05:15:39 +0530

    commit

---
+++ src/app/page.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+const Homepage = () => {
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* IMAGE CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+          <Image src="/hero.png" alt="" fill className="object-contain" />
+        </div>
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+          {/* TITLE */}
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Aspiring Software Engineer with interest in Machine Learning.
+          </h1>
+          {/* DESC */}
+          <p className="md:text-xl">
+            Welcome to my corner of the internet, My journey as a budding software developer shines 
+            through my diverse range of projects. From crafting smart algorithms to working with data, 
+            my portfolio reflects my commitment to quality and my constant thirst for learning in the fast-paced
+            world of tech.
+          </p>
+          {/* BUTTONS */}
+          <div className="w-full flex gap-4">
+            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              View My Work
+            </button>
+            <button className="p-4 rounded-lg ring-1 ring-black">
+              Contact Me
+            </button>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Homepage;
